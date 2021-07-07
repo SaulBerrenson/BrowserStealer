1ab06283b8d50ac0e3ed384088582b326615c960
YankeesGo <64542974+SaulBerrenson@users.noreply.github.com>
2021-07-07 11:42:05 +0300

    main entry-point

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include "ChromeParser.h"
+#include "FirefoxParser.h"
+
+
+int main()
+{
+	
+
+
+	FirefoxParser firefox_parser;
+
+	auto accounts = firefox_parser.collect_data();
+
+	for (const auto& data : accounts)
+	{
+		std::cout << "Url: " << data.Url << std::endl << "Username: " << data.Username << std::endl << "Password: " << data.Password << std::endl;
+		std::cout << "--------------------------------------------------------------------------------" << std::endl;
+	}
+
+	
+	chrome_parser parser;
+	List<AccountData> data_out;
+	if (!parser.try_parse_chrome(data_out))
+	{
+		std::cout << "user data was not found!";
+	}
+
+
+	for (const auto& data : data_out)
+	{
+		std::cout << "Url: " <<data.Url << std::endl << "Username: " << data.Username << std::endl << "Password: " << data.Password << std::endl;
+		std::cout << "--------------------------------------------------------------------------------" << std::endl;
+	}
+	
+	system("pause");
+	return 0;
+}
