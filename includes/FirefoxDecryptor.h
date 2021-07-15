a9717dc3f4082704a4693ed03a2f671182bd93a9
YankeesGo <64542974+SaulBerrenson@users.noreply.github.com>
2021-07-15 16:09:11 +0300

    refactoring firefox decryptor (impl interface)

--- includes/FirefoxDecryptor.h
+++ includes/FirefoxDecryptor.h
@@ -1,5 +1,6 @@
 ﻿#pragma once
 #include "Forwards.h"
+#include "IDecryptor.h"
 
 enum class SECItemType {
     siBuffer = 0,
@@ -28,20 +29,22 @@ using NssInit = long(char* sDirectory);
 using NssShutdown = long();
 
 
-class FirefoxDecryptor
+class FirefoxDecryptor : public IDecryptor
 {
 public:
-    ~FirefoxDecryptor();
+	FirefoxDecryptor() = default;
+    ~FirefoxDecryptor() override;
 	
-	bool init(const String& sPath);
-    bool set_profile_dir(const String& profile_dir);
-    bool decrypt_data(const String& encrypted_data, String& decrypted_data);
-	
-    NssInit* m_NssInit = nullptr;
-    Pk11SdrDecrypt* m_ipNssPk11SdrDecrypt = nullptr;
-    NssShutdown* m_NssShutdown = nullptr;
+	bool init(const String& sPath) override;    
+    bool decrypt_data(const String& encrypted_data, String& decrypted_data) override;
 
-   HMODULE m_hNss3 = nullptr;
-   HMODULE m_hMozGlue = nullptr;
+    bool set_profile_dir(const String& profile_dir);
+
+private:
+	NssInit* m_NssInit = nullptr;
+	Pk11SdrDecrypt* m_ipNssPk11SdrDecrypt = nullptr;
+	NssShutdown* m_NssShutdown = nullptr;
+	HMODULE m_hNss3 = nullptr;
+	HMODULE m_hMozGlue = nullptr;
 	
 };
